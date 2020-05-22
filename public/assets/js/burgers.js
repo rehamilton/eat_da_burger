524140e5842c9eba9de2a147694bdcdbbe06a033
Roz <roz_E4@hotmail.com>
2020-05-22 12:30:02 +0800

    fix errors created during markup

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,32 +1,32 @@
 $(function() {
-    $(".change-state").on("click", event => {
+    $(".change-state").on("click", function(event) {
         //identify record to be targeted
-        let id = $(this).data("id");
-        let newEaten = $(this).data("newstate");
+       let id = $(this).data("id");
+       let newEaten = $(this).data("newstate");
 
-
-        let newEatenState = {
-        eaten: newEaten
+    
+       let newEatenState = {
+            eaten: newEaten
         };
 
         console.log(newEaten);
-
+    
         // Send the PUT request.
         $.ajax("/api/burgers/" + id, {
-        type: "PUT",
-        data: newEatenState
+            type: "PUT",
+            data: newEatenState
         }).then(
-        function() {
+            function() {
             console.log("changed state to", newEaten);
             
             location.reload();
-        }
+            }
         );
     });
-
+  
     //Submit to POST
-    $(".burger-form").on("submit", event => {
-        
+    $(".burger-form").on("submit", function(event) {
+      
         event.preventDefault();
         //Get data to be sent to database
         let newBurger = {
@@ -34,7 +34,7 @@ $(function() {
             eaten: 0
         };
 
-        // Send the POST request
+        // Send the POST request.
         $.ajax("/api/burgers", {
             type: "POST",
             data: newBurger
@@ -45,21 +45,20 @@ $(function() {
             location.reload();
             }
         );
-        });
-
-        
-    //Get rid of burger - DELETE 
-    $(".delete-burger").on("click", event => {
+    });
+  
+    //Throw out button - DELETE
+    $(".delete-burger").on("click", function(event) {
         //identify record to be targeted
-        let id = $(this).data("id");
-
-        // Send DELETE request
+        var id = $(this).data("id");
+    
+        // Send the DELETE request.
         $.ajax("/api/burgers/" + id, {
             type: "DELETE"
         }).then(
             function() {
             console.log("deleted burger", id);
-            
+            // Reload
             location.reload();
             }
         );
