9ad0a734f71c06f82e63792df0c273807a45d08f
Roz <roz_E4@hotmail.com>
2020-05-22 11:18:28 +0800

    connection changes for Heroku Jaws DB

--- config/connections.js
+++ config/connections.js
@@ -1,14 +1,18 @@
 //put sql connections here
 
-var mysql = require("mysql");
+const mysql = require("mysql");
 
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else{
+const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "root",
   database: "eatdaburger_db"
 });
+};
 
 
 connection.connect(function(err) {
